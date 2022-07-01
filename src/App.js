c894773d4fc0dd5f2a8ea9cdbc6ea44d3b5a0936
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-06-30 20:17:46 -0400

    added a line

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload the page.
-        </p>
+        <p>Hello World!</p>
       </header>
     </div>
   );
