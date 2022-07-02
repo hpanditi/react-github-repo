4ff3254f81f2b57f8163674a114fe87fd9d44877
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-01 21:43:37 -0400

    added file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello React!</p>
+        <p>Hello React! This is the most updated version.</p>
       </header>
     </div>
   );
