6fc691a084b9033d0044d9138434cb81c9895341
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-02 11:07:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
       <header className="App-header">
         <h1>Deets Cafe</h1>
       </header>
+      <body>
+        <p>Welcome to this coffee shop!</p>
+      </body>
     </div>
   );
 }
