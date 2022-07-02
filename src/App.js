5535af5b48d61b03f2a947731f0baf6b9bd9170e
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-02 11:00:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Deets Coding Cafe</p>
+        <h1>Deets Cafe</h1>
       </header>
     </div>
   );
