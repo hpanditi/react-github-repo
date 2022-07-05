514dbfcf2d8819fa5d170dc5aa8fd96196a4da12
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-05 18:37:02 -0400

    add

--- src/components/Items.js
+++ src/components/Items.js
@@ -22,34 +22,37 @@ function Items() {
         <img
           src={light}
           alt="lightroast"
-          style={{ width: "250px", height: "250 px" }}
+          style={{ width: "250px", height: "250 px", padding: "5px" }}
         />
         <p>Light Roast $12.95/pound</p>
         <Button>Add to cart</Button>
         <Button>Learn more</Button>
+        <p></p>
         <br />
         <img
           src={medium}
           alt="mediumroast"
-          style={{ width: "250px", height: "250 px" }}
+          style={{ width: "250px", height: "250 px", padding: "5px" }}
         />
         <p>Medium Roast $12.95/pound</p>
         <Button>Add to cart</Button>
         <Button>Learn more</Button>
+        <p></p>
         <br />
         <img
           src={dark}
           alt="darkroast"
-          style={{ width: "250px", height: "250 px" }}
+          style={{ width: "250px", height: "250 px", padding: "5px" }}
         />
         <p>Dark Roast $12.95/pound</p>
         <Button>Add to cart</Button>
         <Button>Learn more</Button>
+        <p></p>
         <br />
         <img
           src={extradark}
           alt="extradarkroast"
-          style={{ width: "250px", height: "250 px" }}
+          style={{ width: "250px", height: "250 px", padding: "5px" }}
         />
         <p>Extra Dark Roast $12.95/pound</p>
         <Button>Add to cart</Button>
