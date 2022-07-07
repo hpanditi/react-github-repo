fd7551b080b4818706b1609647d8afc94e01d511
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-06 21:15:23 -0400

    add

--- src/components/Items.js
+++ src/components/Items.js
@@ -6,9 +6,7 @@ import dark from "./darkroast.jpeg";
 import extradark from "./extradarkroast.jpeg";
 
 function Items() {
-  this.handleClick = () => {
-    alert("Item added to cart!");
-  };
+  const handleLightAddToCart = () => alert("Added to cart!");
   return (
     <div>
       <header>
@@ -28,7 +26,7 @@ function Items() {
           style={{ width: "250px", height: "250 px", padding: "5px" }}
         />
         <p>Light Roast $12.95/pound</p>
-        <button className="light-add-to-cart" onClick={this.handleClick}>
+        <button className="light-add-to-cart" onClick={handleLightAddToCart}>
           Add to cart
         </button>
         <button className="light-learn-more">Learn more</button>
