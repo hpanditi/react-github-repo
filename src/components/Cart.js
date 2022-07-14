9d9d11d6f775a9c6375ccabf0303e9e4ef581586
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-14 16:11:16 -0400

    add

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -113,74 +113,74 @@ class Total extends React.Component {
 }
 
 /* ProductForm */
-class ProductForm extends React.Component {
-  submit(e) {
-    e.preventDefault();
-    var product = {
-      name: this.refs.name.value,
-      price: Number(this.refs.price.value),
-      info: this.refs.info.value,
-    };
-    console.log(this.refs.name.value, this.refs.price.value);
-    this.props.handleProduct(product);
-    this.refs.name.value = "";
-    this.refs.price.value = 0;
-    this.refs.info.value = "";
-  }
+// class ProductForm extends React.Component {
+//   submit(e) {
+//     e.preventDefault();
+//     var product = {
+//       name: this.refs.name.value,
+//       price: Number(this.refs.price.value),
+//       info: this.refs.info.value,
+//     };
+//     console.log(this.refs.name.value, this.refs.price.value);
+//     this.props.handleProduct(product);
+//     this.refs.name.value = "";
+//     this.refs.price.value = 0;
+//     this.refs.info.value = "";
+//   }
 
-  render() {
-    return (
-      <form onSubmit={this.submit.bind(this)}>
-        <h3>add new product</h3>
-        <div className="row form-group">
-          <label className="col-sm-2  col-sm-form-label">name:</label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              className="form-control"
-              ref="name"
-              placeholder="e.g.) coffee roast"
-              required
-            />
-          </div>
-        </div>
+//   render() {
+//     return (
+//       <form onSubmit={this.submit.bind(this)}>
+//         <h3>add new product</h3>
+//         <div className="row form-group">
+//           <label className="col-sm-2  col-sm-form-label">name:</label>
+//           <div className="col-sm-10">
+//             <input
+//               type="text"
+//               className="form-control"
+//               ref="name"
+//               placeholder="e.g.) coffee roast"
+//               required
+//             />
+//           </div>
+//         </div>
 
-        <div className="row form-group">
-          <label className="col-sm-2  col-sm-form-label">price:</label>
-          <div className="col-sm-10">
-            <input
-              type="number"
-              className="form-control"
-              ref="price"
-              placeholder="e.g.) $12"
-              required
-            />
-          </div>
-        </div>
+//         <div className="row form-group">
+//           <label className="col-sm-2  col-sm-form-label">price:</label>
+//           <div className="col-sm-10">
+//             <input
+//               type="number"
+//               className="form-control"
+//               ref="price"
+//               placeholder="e.g.) $12"
+//               required
+//             />
+//           </div>
+//         </div>
 
-        <div className="row form-group">
-          <label className="col-sm-2  col-sm-form-label">info:</label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              className="form-control"
-              ref="info"
-              placeholder="e.g.) special roast"
-            />
-          </div>
-        </div>
+//         <div className="row form-group">
+//           <label className="col-sm-2  col-sm-form-label">info:</label>
+//           <div className="col-sm-10">
+//             <input
+//               type="text"
+//               className="form-control"
+//               ref="info"
+//               placeholder="e.g.) special roast"
+//             />
+//           </div>
+//         </div>
 
-        <div className="row form-group">
-          <div className="offset-2 col-10">
-            <button className="btn btn-outline-primary">create product</button>
-          </div>
-        </div>
+//         <div className="row form-group">
+//           <div className="offset-2 col-10">
+//             <button className="btn btn-outline-primary">create product</button>
+//           </div>
+//         </div>
 
-        <hr />
-      </form>
-    );
-  }
-}
+//         <hr />
+//       </form>
+//     );
+//   }
+// }
 
 /* ProductList */
 class ProductList extends React.Component {
@@ -239,7 +239,7 @@ class ProductList extends React.Component {
 
     return (
       <div>
-        <ProductForm handleProduct={this.createProduct} />
+        {/* <ProductForm handleProduct={this.createProduct} /> */}
         {products}
         <Total total={this.state.total} />
       </div>
@@ -262,7 +262,7 @@ function Cart() {
       <body>
         <Product />
         <Total />
-        <ProductForm />
+        {/* <ProductForm /> */}
         <ProductList />
       </body>
     </div>
