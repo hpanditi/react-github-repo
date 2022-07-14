5405b708158b023c79855adb7990d390925cec30
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-14 13:49:32 -0400

    add

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+console.clear();
+
+let productList = [
+  { name: "light roast", price: 12.95, info: "light roast coffee" },
+  { name: "medium roast", price: 12.95, info: "medium roast coffee" },
+  { name: "dark roast", price: 12.95, info: "dark roast coffee" },
+  { name: "extra dark roast", price: 12.95, info: "extra dark roast coffee" },
+];
+
 function Cart() {
   return (
     <div>
@@ -12,7 +21,41 @@ function Cart() {
         <a href="/Contact"> Contact</a>
         <h1>Cart</h1>
       </header>
-      <body></body>
+      <body>
+        <div>
+          <div className="row form-group">
+            <div className="col-sm-10">
+              <h4>
+                {this.props.name}: ${this.props.price}
+              </h4>
+            </div>
+            <div className="col-sm-2 text-right">qty: {this.state.qty}</div>
+          </div>
+          <div className="row btn-toolbar">
+            <div className="col-6">
+              <button
+                className="btn btn-outline-primary"
+                onClick={this.showInfo}
+              >
+                show info
+              </button>
+            </div>
+            <div className="col-6 text-right">
+              <button className="btn btn-outline-primary" onClick={this.add}>
+                +1
+              </button>
+              <button
+                className="btn btn-outline-primary"
+                onClick={this.subtract}
+                disabled={this.state.qty < 1}
+              >
+                -1
+              </button>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </body>
     </div>
   );
 }
