569d5c66b4a41f0fde6f3b0fd461fb31043da312
Hemanth Panditi <panditih@mail.gvsu.edu>
2022-07-15 14:59:03 -0400

    add

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,21 @@ function Contact() {
           would love to hear from you!
         </p>
         <br />
-        
+        <form>
+          <div style={{ padding: 20 }}>
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" required />
+          </div>
+          <div style={{ padding: 25 }}>
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email" required />
+          </div>
+          <div>
+            <label htmlFor="message">Message: </label>
+            <textarea id="message" required />
+          </div>
+          <button type="submit">submit</button>
+        </form>
       </body>
     </div>
   );
